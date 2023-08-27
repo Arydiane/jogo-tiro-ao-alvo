20ab96977cc75e97168faa53fb9583502cacc239
Arydiane <arydianejardim@gmail.com>
2023-08-27 10:29:43 -0300

    cria função sorteia posição

--- js/script.js
+++ js/script.js
@@ -2,7 +2,8 @@ const canvas = document.getElementById('game-board');
 const context = canvas.getContext('2d');
 
 const raio = 10; 
-
+let xAleatorio;
+let yAleatorio;
 
 function desenhaCirculo(x, y, raio, cor) {
     context.fillStyle = cor;
@@ -17,3 +18,22 @@ function desenhaAlvo(x, y) {
     desenhaCirculo(x, y, raio, '#ff0000');
 }
 
+function sorteiaPosicao(minimo, maximo) {
+    return Math.floor( minimo + Math.random() * (maximo - minimo + 1));
+}
+
+function limpaTela() {
+    context.clearRect(0, 0, 600, 400);
+}
+
+function atualizaTela() {
+    limpaTela();
+    //apenas sorteia posições que ficaram dentro da tela
+    //30 é raio do circulo maior do alvo
+    xAleatorio = sorteiaPosicao(30, canvas.width - 30);
+    yAleatorio = sorteiaPosicao(30, canvas.height - 30);
+
+    desenhaAlvo(xAleatorio, yAleatorio);
+}
+
+atualizaTela(); 
